85f70ddce89ee061a8e14351088b61faad54b8ec
portbound <token.api.guy@gmail.com>
2026-01-23 22:04:27 -0500

    finished encodeRecord() test

--- main_test.go
+++ main_test.go
@@ -1,60 +1,53 @@
 package main
 
 import (
+	"bytes"
+	"encoding/binary"
+	"hash/crc32"
 	"testing"
 )
 
-func TestBitcask_Put(t *testing.T) {
-	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for receiver constructor.
-		path string
-		// Named input parameters for target function.
-		k       []byte
-		v       []byte
-		wantErr bool
-	}{
-		// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			b, err := NewBitcask(tt.path)
-			if err != nil {
-				t.Fatalf("could not construct receiver type: %v", err)
-			}
-			gotErr := b.Put(tt.k, tt.v)
-			if gotErr != nil {
-				if !tt.wantErr {
-					t.Errorf("Put() failed: %v", gotErr)
-				}
-				return
-			}
-			if tt.wantErr {
-				t.Fatal("Put() succeeded unexpectedly")
-			}
-		})
-	}
-}
-
-
 func Test_encodeRecord(t *testing.T) {
+	const testTimestamp = 1674496800
 	tests := []struct {
-		name string // description of this test case
-		// Named input parameters for target function.
+		name string
 		k    []byte
 		v    []byte
 		want []byte
 	}{
-		// TODO: Add test cases.
+		{
+			name: "valid",
+			k:    []byte("key"),
+			v:    []byte("value"),
+			want: func() []byte {
+				keyLen := uint32(len([]byte("key")))
+				valueLen := uint32(len([]byte("value")))
+
+				buf := make([]byte, 16+keyLen+valueLen)
+				offset := 4
+				binary.BigEndian.PutUint32(buf[offset:], uint32(testTimestamp))
+				offset += 4
+				binary.BigEndian.PutUint32(buf[offset:], uint32(keyLen))
+				offset += 4
+				binary.BigEndian.PutUint32(buf[offset:], uint32(valueLen))
+				offset += 4
+				copy(buf[offset:], []byte("key"))
+				offset += int(keyLen)
+				copy(buf[offset:], []byte("value"))
+
+				checksum := crc32.ChecksumIEEE(buf[4:])
+				binary.BigEndian.PutUint32(buf[:4], checksum)
+
+				return buf
+			}(),
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got := encodeRecord(tt.k, tt.v)
-			// TODO: update the condition below to compare got with tt.want.
-			if true {
-				t.Errorf("encodeRecord() = %v, want %v", got, tt.want)
+			got := encodeRecord(tt.k, tt.v, testTimestamp)
+			if !bytes.Equal(got, tt.want) {
+				t.Errorf("expected byte slice equality, got:%v want%v", got, tt.want)
 			}
 		})
 	}
 }
-
