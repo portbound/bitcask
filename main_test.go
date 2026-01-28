fe71aa734708a6fa94c2622b2d21051aae0da57c
portbound <token.api.guy@gmail.com>
2026-01-27 21:31:12 -0500

    updated PUT() test cases to include one to check datafile rotation

--- main_test.go
+++ main_test.go
@@ -59,21 +59,39 @@ func TestBitcask_Put(t *testing.T) {
 		k       []byte
 		v       []byte
 		wantErr bool
+		setup   func(t *testing.T) *Bitcask
 	}{
 		{
-			name:    "passing",
+			name:    "vanilla: passing",
 			k:       []byte("key"),
 			v:       []byte("value"),
 			wantErr: false,
+			setup: func(t *testing.T) *Bitcask {
+				b, err := NewBitcask(t.TempDir())
+				if err != nil {
+					t.Fatalf("could not construct receiver type: %v", err)
+				}
+				return b
+			},
+		},
+		{
+			name:    "rotate datafile: passing",
+			k:       []byte("key"),
+			v:       []byte("value"),
+			wantErr: false,
+			setup: func(t *testing.T) *Bitcask {
+				b, err := NewBitcaskWithSizeLimit(t.TempDir(), 1)
+				if err != nil {
+					t.Fatalf("could not construct receiver type: %v", err)
+				}
+				return b
+			},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			b, err := NewBitcask(t.TempDir())
-			if err != nil {
-				t.Fatalf("could not construct receiver type: %v", err)
-			}
+			b := tt.setup(t)
 			gotErr := b.Put(tt.k, tt.v)
 			if gotErr != nil {
 				if !tt.wantErr {
@@ -96,7 +114,7 @@ func TestBitcask_Get(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name:    "passing",
+			name:    "vanilla: passing",
 			key:     []byte("key"),
 			want:    []byte("value"),
 			wantErr: false,
